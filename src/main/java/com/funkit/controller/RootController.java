4cbccb8d5388f19f840e2e8bed869c9201555ccd
hojae29 <gkqthd6386@naver.com>
2022-04-14 09:11:05 +0900

    first commit

---
+++ src/main/java/com/funkit/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.funkit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+}
