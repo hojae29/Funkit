18c8a4fa1065cdad9c196cf14dd4ce6e1e1136ee
hojae29 <gkqthd6386@naver.com>
2022-04-19 16:33:04 +0900

    feat : Member 서버 유효성검증 Validation 의존성 추가 서버에서 회원가입폼 유효성 검사 구현

--- src/main/java/com/funkit/model/Member.java
+++ src/main/java/com/funkit/model/Member.java
@@ -1,14 +1,50 @@
 package com.funkit.model;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Member {
+    @NotEmpty(message = "아이디를 입력해주세요")
+    @Pattern(
+            regexp = "^[A-za-z0-9]{4,12}$",
+            message = "아이디는 4~12자리 영문으로 입력해야 합니다"
+    )
     private String id;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$",
+            message = "비밀번호 형식이 맞지 않습니다(최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자)"
+    )
     private String passwd;
+
+    @NotEmpty(message = "이름을 입력해주세요")
     private String name;
+
+    @NotEmpty(message = "닉네임을 입력해주세요")
+    @Size(max = 8, message = "닉네임은 8자리이하여야 합니다")
     private String nickName;
+
+    @NotEmpty(message = "전화번호를 입력해주세요")
+    @Pattern(
+            regexp = "^(01[016789]{1}|02|0[3-9]{1}\\d{1})-?\\d{3,4}-?\\d{4}$",
+            message = "잘못된 전화번호 형식입니다"
+    )
     private String phone;
+
+    @NotEmpty(message = "이메일을 입력해주세요")
+    @Email(message = "잘못된 이메일 형식입니다")
     private String email;
+
+    @NotEmpty(message = "주소를 입력해주세요")
     private String postCode;
+
+    @NotEmpty(message = "주소를 입력해주세요")
     private String address;
+
     private String detailAddress;
 
     public String getId() {
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -46,12 +46,20 @@ document.getElementById("check_id_btn").addEventListener("click", () => {
 document.getElementById("submit_register_btn").addEventListener("click", () => {
     const form = document.getElementById("register_form");
 
-    const regex_pw = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
-    const regex_e = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-    const regex_tel = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+    //최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자
+    const regex_pw = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+    const regex_e = /^([0-9a-zA-Z_\.-]+)@([\da-zA-Z_-]+)(\.[\da-zA-Z_-]+){1,2}$/;
+    const regex_tel = /^(01[016789]{1}|02|0[3-9]{1}\d{1})-?\d{3,4}-?\d{4}$/;
+    //4 ~ 12자리 영문
+    const regex_id = /{^[A-za-z\d]{4,12}$/;
 
     if(form.id.value === ""){
         alert("아이디를 입력해주세요");
+        form.id.focus();
+        return false;
+    }
+    if(!regex_id.test(form.id.value)){
+        alert("아이디는 4~12자리 영문으로 입력해야 합니다");
         form.id.focus();
         return false;
     }
@@ -69,7 +77,7 @@ document.getElementById("submit_register_btn").addEventListener("click", () => {
         return false;
     }
     if(!regex_pw.test(form.passwd.value)){
-        alert("비밀번호 형식을 확인해주세요");
+        alert("비밀번호 형식이 맞지 않습니다(최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자)");
         form.passwd.focus();
         return false;
     }
@@ -78,14 +86,14 @@ document.getElementById("submit_register_btn").addEventListener("click", () => {
         form.checkPasswd.focus();
         return false;
     }
-    if(form.name.value == ""){
-        alert("이름을 입력해주세요");
-        form.name.focus();
-        return false;
-    }
     if(form.passwd.value != form.checkPasswd.value){
         alert("비밀번호 확인이 일치하지 않습니다");
         form.checkPasswd.focus();
+        return false;
+    }
+    if(form.name.value == ""){
+        alert("이름을 입력해주세요");
+        form.name.focus();
         return false;
     }
     if(form.nickName.value == ""){
@@ -109,7 +117,7 @@ document.getElementById("submit_register_btn").addEventListener("click", () => {
         return false;
     }
     if(!regex_e.test(form.email.value)){
-        alert("이메일 형식을 확인해주세요");
+        alert("잘못된 이메일 형식입니다");
         form.email.focus();
         return false;
     }
@@ -135,10 +143,20 @@ document.getElementById("submit_register_btn").addEventListener("click", () => {
         contentType: "application/json",
         data: JSON.stringify(member),
         success: res => alert(res.message),
-        error: res => alert(res.responseJSON.message)
+        error: res => {
+            res = res.responseJSON;
+            console.log(res);
+            if(res.status == 409){
+                alert(res.message);
+            }else if(res.status == 400){
+                for (const [key, value] of Object.entries(res.data)) {
+                    console.log(`${key} : ${value}`);
+                }
+            }
+        }
     });
 
-    //form data reset
-    form.checkId.value = "";
-    form.reset();
+    //폼 데이터 초기화 코드
+    // form.checkId.value = "";
+    // form.reset();
 });
